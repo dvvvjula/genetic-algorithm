cd43a98647d7ec3f6ea5f58988ea4c8510fc1c7a
1wakos <ciapalajulka@gmail.com>
2025-06-04 15:04:36 +0200

    add mutation operator, fitness evaluation

--- genetic.c
+++ genetic.c
@@ -12,5 +12,55 @@ int isSolved(int *genes, int size);
 
 void solveWithGenetic(int **puzzle, int size, int selectionMethod)
 {
-    // for later
+    int chromosomeLength = size * size;
+    Population *population = malloc(sizeof(Population));
+    population->individuals = malloc(sizeof(Individual *) * POPULATION_SIZE);
+
+    for (int i = 0; i < POPULATION_SIZE; i++)
+    {
+        population->individuals[i] = malloc(sizeof(Individual));
+        population->individuals[i]->genes = malloc(sizeof(int) * chromosomeLength);
+        for (int j = 0; j < chromosomeLength; j++)
+        {
+            population->individuals[i]->genes[j] = puzzle[j / size][j % size];
+            if (population->individuals[i]->genes[j] == 0)
+                population->individuals[i]->genes[j] = (rand() % size) + 1;
+        }
+        population->individuals[i]->fitness = getFitness(population->individuals[i]->genes, size);
+    }
+}
+
+int getFitness(int *genes, int size)
+{
+    int fitness = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        int *row = calloc(size + 1, sizeof(int));
+        int *col = calloc(size + 1, sizeof(int));
+        for (int j = 0; j < size; j++)
+        {
+            row[genes[i * size + j]]++;
+            col[genes[j * size + i]]++;
+        }
+        for (int k = 1; k <= size; k++)
+        {
+            if (row[k] == 1)
+                fitness++;
+            if (col[k] == 1)
+                fitness++;
+        }
+        free(row);
+        free(col);
+    }
+    return fitness;
+}
+
+void mutate(int *genes, int size)
+{
+    int i = rand() % (size * size);
+    int j = rand() % (size * size);
+    int temp = genes[i];
+    genes[i] = genes[j];
+    genes[j] = temp;
 }
